12003afff3517e6489b8369ba218a317808e47b4
whoissubedibibek <subedibibek00@outlook.com>
2022-01-11 11:14:41 +0545

    simple higher order function

---
+++ calculator.js
@@ -0,0 +1,25 @@
+var num1;
+var num2;
+
+function calculator(num1, num2 , operator){
+    return operator(num1,num2);
+}
+
+function add(num1, num2){
+    return num1+num2 ;
+}
+
+function sub(num1, num2){
+    return num1-num2 ;
+
+}
+
+function multiply(num1, num2){
+    return num1*num2 ;
+
+}
+
+function divide(num1, num2){
+    return num1/num2 ;
+
+}
