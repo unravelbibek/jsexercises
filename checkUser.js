036115096f04947f10b619ebfd131edc372e66d2
whoissubedibibek <subedibibek00@outlook.com>
2022-01-20 15:03:41 +0545

    check user program

---
+++ checkUser.js
@@ -0,0 +1,35 @@
+/*
+Write the code which asks for a login with prompt.
+
+If the visitor enters "Admin", then prompt for a password, 
+if the input is an empty line or Esc – show “Canceled”, 
+if it’s another string – then show “I don’t know you”.
+
+The password is checked as follows:
+
+If it equals “TheMaster”, then show “Welcome!”,
+Another string – show “Wrong password”,
+For an empty string or cancelled input, show “Canceled”
+
+*/
+
+
+let userName = prompt("Who's there?", '');
+
+if (userName === 'Admin') {
+
+  let pass = prompt('Password?', '');
+
+  if (pass === 'TheMaster') {
+    alert( 'Welcome!' );
+  } else if (pass === '' || pass === null) {
+    alert( 'Canceled' );
+  } else {
+    alert( 'Wrong password' );
+  }
+
+} else if (userName === '' || userName === null) {
+  alert( 'Canceled' );
+} else {
+  alert( "I don't know you" );
+}
