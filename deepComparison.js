68d40b49dfbdb72b2c7b34be633e9549675a3bed
Subedi Bibek <pingsubedibibek@gmail.com>
2022-03-10 00:39:53 +0545

    new files added

--- deepComparison.js
+++ deepComparison.js
@@ -1,13 +1,30 @@
-const object1 = {
-    a: 'somestring',
-    b: 42,
-    c: false
-  };
+function deepComparison(first, second, indentation='') {
+    if (typeof first === typeof second) {
+        if (typeof first === "object") {
+      if (first !== null) {
+        firstKeys = Object.keys(first);
+        secondKeys = Object.keys(second);
+        if (firstKeys.length == secondKeys.length) {
+          trackInequalities = [];
+          for (i=0; i<=firstKeys.length-1; i++) {
+            trackInequalities.push(deepComparison(first[firstKeys[i]],second[secondKeys[i]], '    '));
+          }
+          return !trackInequalities.includes(false)
+        } else {
+          
+          return false;
+        }  
+      } else {
+        
+        return true; 
+      }
+        } else {
+            return first === second
+        }
+    } else {
+        return false;
+    }
+}
 
-  const object2 = {
-    a: 'somestring',
-    b: 42,
-    c: false
-  };
-
-// working 
+console.log(deepComparison({bibek:'subedi'},{bibek:'subedi'}))
+console.log(deepComparison({bibek:'dai'},{bibek:'subedi'}))
